8bb99b5c484344ba58c60cc0f14b890bea6f350c
Tony0812-dev <tony@kemono-network.com>
2022-12-19 21:00:50 +0800

    Create cf2dns_actions_v6.py

---
+++ cf2dns_actions_v6.py
@@ -0,0 +1,184 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import random
+import time
+import json
+import urllib3
+import os
+import traceback
+from dns.aliyun import AliApi
+
+KEY = os.environ["KEY"]
+#CM:移动 CU:联通 CT:电信 AB:境外 DEF:默认
+#修改需要更改的dnspod域名和子域名
+DOMAINS = json.loads(os.environ["DOMAINS"])
+SECRETID = os.environ["SECRETID"]
+SECRETKEY = os.environ["SECRETKEY"]
+AFFECT_NUM = 2
+DNS_SERVER = 2
+#如果使用阿里云解析 REGION出现错误再修改 默认不需要修改 https://help.aliyun.com/document_detail/198326.html
+REGION_ALI = 'cn-hongkong'
+TTL = 1
+TYPE = 'v6'
+
+urllib3.disable_warnings()
+
+def get_optimization_ip():
+    try:
+        http = urllib3.PoolManager()
+        headers = headers = {'Content-Type': 'application/json'}
+        data = {"key": KEY, "type": TYPE}
+        data = json.dumps(data).encode()
+        response = http.request('POST','https://api.hostmonit.com/get_optimization_ip',body=data, headers=headers)
+        return json.loads(response.data.decode('utf-8'))
+    except Exception as e:
+        print(traceback.print_exc())
+        return None
+
+def changeDNS(line, s_info, c_info, domain, sub_domain, cloud):
+    global AFFECT_NUM, TYPE
+    if TYPE == 'v6':
+        recordType = "AAAA"
+    else:
+        recordType = "A"
+    
+    if line == "CM":
+        line = "移动"
+    elif line == "CU":
+        line = "联通"
+    elif line == "CT":
+        line = "电信"
+    elif line == "AB":
+        line = "境外"
+    elif line == "DEF":
+        line = "默认"
+    else:
+        print("CHANGE DNS ERROR: ----Time: " + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + "----MESSAGE: LINE ERROR")
+        return
+    try:
+        create_num = AFFECT_NUM - len(s_info)
+        if create_num == 0:
+            for info in s_info:
+                if len(c_info) == 0:
+                    break
+                cf_ip = c_info.pop(random.randint(0,len(c_info)-1))["ip"]
+                if cf_ip in str(s_info):
+                    continue
+                ret = cloud.change_record(domain, info["recordId"], sub_domain, cf_ip, recordType, line, TTL)
+                if(DNS_SERVER != 1 or ret["code"] == 0):
+                    print("CHANGE DNS SUCCESS: ----Time: " + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + "----DOMAIN: " + domain + "----SUBDOMAIN: " + sub_domain + "----RECORDLINE: "+line+"----RECORDID: " + str(info["recordId"]) + "----VALUE: " + cf_ip )
+                else:
+                    print("CHANGE DNS ERROR: ----Time: " + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + "----DOMAIN: " + domain + "----SUBDOMAIN: " + sub_domain + "----RECORDLINE: "+line+"----RECORDID: " + str(info["recordId"]) + "----VALUE: " + cf_ip + "----MESSAGE: " + ret["message"] )
+        elif create_num > 0:
+            for i in range(create_num):
+                if len(c_info) == 0:
+                    break
+                cf_ip = c_info.pop(random.randint(0,len(c_info)-1))["ip"]
+                if cf_ip in str(s_info):
+                    continue
+                ret = cloud.create_record(domain, sub_domain, cf_ip, recordType, line, TTL)
+                if(DNS_SERVER != 1 or ret["code"] == 0):
+                    print("CREATE DNS SUCCESS: ----Time: " + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + "----DOMAIN: " + domain + "----SUBDOMAIN: " + sub_domain + "----RECORDLINE: "+line+"----VALUE: " + cf_ip )
+                else:
+                    print("CREATE DNS ERROR: ----Time: " + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + "----DOMAIN: " + domain + "----SUBDOMAIN: " + sub_domain + "----RECORDLINE: "+line+"----RECORDID: " + str(info["recordId"]) + "----VALUE: " + cf_ip + "----MESSAGE: " + ret["message"] )
+        else:
+            for info in s_info:
+                if create_num == 0 or len(c_info) == 0:
+                    break
+                cf_ip = c_info.pop(random.randint(0,len(c_info)-1))["ip"]
+                if cf_ip in str(s_info):
+                    create_num += 1
+                    continue
+                ret = cloud.change_record(domain, info["recordId"], sub_domain, cf_ip, recordType, line, TTL)
+                if(DNS_SERVER != 1 or ret["code"] == 0):
+                    print("CHANGE DNS SUCCESS: ----Time: " + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + "----DOMAIN: " + domain + "----SUBDOMAIN: " + sub_domain + "----RECORDLINE: "+line+"----RECORDID: " + str(info["recordId"]) + "----VALUE: " + cf_ip )
+                else:
+                    print("CHANGE DNS ERROR: ----Time: " + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + "----DOMAIN: " + domain + "----SUBDOMAIN: " + sub_domain + "----RECORDLINE: "+line+"----RECORDID: " + str(info["recordId"]) + "----VALUE: " + cf_ip + "----MESSAGE: " + ret["message"] )
+                create_num += 1
+    except Exception as e:
+            print("CHANGE DNS ERROR: ----Time: " + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + "----MESSAGE: " + str(traceback.print_exc()))
+
+def main(cloud):
+    global AFFECT_NUM, TYPE
+    if TYPE == 'v6':
+        recordType = "AAAA"
+    else:
+        recordType = "A"
+    if len(DOMAINS) > 0:
+        try:
+            cfips = get_optimization_ip()
+            if cfips == None or cfips["code"] != 200:
+                print("GET CLOUDFLARE IP ERROR: ----Time: " + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) )
+                return
+            cf_cmips = cfips["info"]["CM"]
+            cf_cuips = cfips["info"]["CU"]
+            cf_ctips = cfips["info"]["CT"]
+            for domain, sub_domains in DOMAINS.items():
+                for sub_domain, lines in sub_domains.items():
+                    temp_cf_cmips = cf_cmips.copy()
+                    temp_cf_cuips = cf_cuips.copy()
+                    temp_cf_ctips = cf_ctips.copy()
+                    temp_cf_abips = cf_ctips.copy()
+                    temp_cf_defips = cf_ctips.copy()
+                    if DNS_SERVER == 1:
+                        ret = cloud.get_record(domain, 20, sub_domain, "CNAME")
+                        if ret["code"] == 0:
+                            for record in ret["data"]["records"]:
+                                if record["line"] == "移动" or record["line"] == "联通" or record["line"] == "电信":
+                                    retMsg = cloud.del_record(domain, record["id"])
+                                    if(retMsg["code"] == 0):
+                                        print("DELETE DNS SUCCESS: ----Time: "  + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + "----DOMAIN: " + domain + "----SUBDOMAIN: " + sub_domain + "----RECORDLINE: "+record["line"] )
+                                    else:
+                                        print("DELETE DNS ERROR: ----Time: "  + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + "----DOMAIN: " + domain + "----SUBDOMAIN: " + sub_domain + "----RECORDLINE: "+record["line"] + "----MESSAGE: " + retMsg["message"] )
+                    ret = cloud.get_record(domain, 100, sub_domain, recordType)
+                    if DNS_SERVER != 1 or ret["code"] == 0 :
+                        if DNS_SERVER == 1 and "Free" in ret["data"]["domain"]["grade"] and AFFECT_NUM > 2:
+                            AFFECT_NUM = 2
+                        cm_info = []
+                        cu_info = []
+                        ct_info = []
+                        ab_info = []
+                        def_info = []
+                        for record in ret["data"]["records"]:
+                            if record["line"] == "移动":
+                                info = {}
+                                info["recordId"] = record["id"]
+                                info["value"] = record["value"]
+                                cm_info.append(info)
+                            if record["line"] == "联通":
+                                info = {}
+                                info["recordId"] = record["id"]
+                                info["value"] = record["value"]
+                                cu_info.append(info)
+                            if record["line"] == "电信":
+                                info = {}
+                                info["recordId"] = record["id"]
+                                info["value"] = record["value"]
+                                ct_info.append(info)
+                            if record["line"] == "境外":
+                                info = {}
+                                info["recordId"] = record["id"]
+                                info["value"] = record["value"]
+                                ab_info.append(info)
+                            if record["line"] == "默认":
+                                info = {}
+                                info["recordId"] = record["id"]
+                                info["value"] = record["value"]
+                                def_info.append(info)
+                        for line in lines:
+                            if line == "CM":
+                                changeDNS("CM", cm_info, temp_cf_cmips, domain, sub_domain, cloud)
+                            elif line == "CU":
+                                changeDNS("CU", cu_info, temp_cf_cuips, domain, sub_domain, cloud)
+                            elif line == "CT":
+                                changeDNS("CT", ct_info, temp_cf_ctips, domain, sub_domain, cloud)
+                            elif line == "AB":
+                                changeDNS("AB", ab_info, temp_cf_abips, domain, sub_domain, cloud)
+                            elif line == "DEF":
+                                changeDNS("DEF", def_info, temp_cf_defips, domain, sub_domain, cloud)
+        except Exception as e:
+            print("CHANGE DNS ERROR: ----Time: " + str(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())) + "----MESSAGE: " + str(traceback.print_exc()))
+
+if __name__ == '__main__':
+    cloud = AliApi(SECRETID, SECRETKEY, REGION_ALI)
+    main(cloud)
